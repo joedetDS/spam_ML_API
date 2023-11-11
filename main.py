c1c8e1c6d7da6ad7cd0f04a80980a67ba2158d8f
Edet Joseph <144073368+joedetDS@users.noreply.github.com>
2023-11-11 10:15:44 +0100

    Files added via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+
+vectorizer = joblib.load("spam_vect.sav")
+spam_model=joblib.load("spam-detector.sav")
+
+app = FastAPI()
+
+#set up the CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+    )
+
+class ModelInput(BaseModel):
+    text: str
+
+
+#Create the end point
+@app.post("/spam_detection")
+def spam_detection(input_parameters: ModelInput):
+    
+    input_mail = input_parameters.text
+    #Preprocess the entered mail
+    v_mail=vectorizer.transform([input_mail])
+    
+    #Predict using the loaded model
+    prediction= spam_model.predict(v_mail)
+    
+    #Check if mail is spam or non-spam
+    if prediction[0]==0:
+        return "Non-Spam"
+    else:
+        return "Spam"
